ae64fffdcf26103e7ff04c786dfb9f794f9d86a4
Romain Reuillon <romain.reuillon@iscpif.fr>
2024-05-09 16:52:21 +0200

    Add test

---
+++ byte-pack/src/test/scala/bytepack/PackTests.scala
@@ -0,0 +1,47 @@
+package bytepack
+
+/*
+ * Copyright (C) 2024 Romain Reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import org.scalatest.funsuite.AnyFunSuite
+
+object PackTests:
+  enum En derives EnumMirror :
+    case V1, V2
+
+  case class TestClass(i: Int, x: Float, e: En, e2: Option[En], e3: Option[En])
+  case class UpperClass(testClass: TestClass, j: Byte)
+
+
+class PackTests extends AnyFunSuite:
+
+  test("Packing and unpacking should return the same case class"):
+    import PackTests.*
+    val p = UpperClass(TestClass(9, 8.0, En.V2, None, Some(En.V1)), 8.toByte)
+    val packed = Pack.pack(p)
+
+    assert(p == Pack.unpack[UpperClass](packed))
+
+  test("Index should be correct"):
+    import PackTests.*
+    val p = UpperClass(TestClass(9, 8.0, En.V2, None, Some(En.V1)), 8.toByte)
+    val packed = Pack.pack(p)
+    assert(Pack.indexOf[UpperClass](1) == 11)
+    assert(packed(Pack.indexOf[UpperClass](1)) == 8.toByte)
+    assert(Pack.indexOf[UpperClass]("j") == 11)
+
